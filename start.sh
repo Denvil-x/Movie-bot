b32ba13faf3c87fc73a85358aec0ac277f9529e3
Denvil-x <112251038+Denvil-x@users.noreply.github.com>
2022-09-17 01:06:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/Denvil-x/Movie-bot.git /Movie-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /Movie-bot
 fi
 cd /EvaMaria
 pip3 install -U -r requirements.txt
