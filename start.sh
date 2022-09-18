2729d1abe7cd650f4577a6c1e22100f68c9532f0
Denvil-x <112251038+Denvil-x@users.noreply.github.com>
2022-09-18 16:50:47 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Movie-bot
 fi
-cd /EvaMaria
+cd /Movie-bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
